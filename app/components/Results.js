6a8a2162a7cc60aaccd9adb4e728a8406117529a
Nick Gorajski <nickgorajski@gmail.com>
2021-06-11 03:21:12 -0500

    Add Results with UI to Battle functionality

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,12 +1,135 @@
 import React from 'react'
+import { battle } from '../utils/api'
+// Notice we're not using a default export, we're using a named import just 
+// to grab the one function.
+import Card from './Card.js'
+import Loading from './Loading.js'
+import Tooltip from './Tooltip.js'
+import PropTypes from 'prop-types'
+import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
+
+function ProfileList({ profile }) {
+	return (
+		<ul className='card-list'>
+			<li>
+				<FaUser color='rgb(239, 115, 115)' size={22} />
+				{profile.name}
+			</li>
+			{profile.location && (
+				<li>
+					<Tooltip text="User's location">
+						<FaCompass color='rgb(144,115,255)' size={22} />
+						{profile.location}
+					</Tooltip>
+				</li>
+			)}
+			{profile.company && (
+				<li>
+					<Tooltip text="User's company">
+						<FaBriefcase color='#795548' size={22} />
+						{profile.company}
+					</Tooltip>
+				</li>
+			)}
+			<li>
+				<FaUsers color='rgb(129, 195, 245)' size={22} />
+				{profile.followers.toLocaleString()} followers
+			</li>
+			<li>
+				<FaUserFriends color='rgb(64, 183, 95)' size={22} />
+				{profile.following.toLocaleString()} following
+			</li>
+		</ul>
+	)
+}
+
+ProfileList.propTypes = {
+	profile: PropTypes.object.isRequired
+}
 
 export default class Results extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			winner: null,
+			loser: null,
+			error: null,
+			loading: true
+		}
+	}
+
+	componentDidMount() {
+		const { playerOne, playerTwo } = this.props
+
+		battle([playerOne, playerTwo])
+			.then((players) => {
+				this.setState({
+					winner: players[0],
+					loser: players[1],
+					error: null,
+					loading: false
+				})
+			}).catch(({ message }) => {
+				this.setState({
+					error: message,
+					loading: false
+				})
+			})
+	}
+
 	render() {
+		const { winner, loser, error, loading } = this.state
+
+		if (loading === true) {
+			return <Loading text='Battling' />
+		}
+
+		if (loading === true) {
+			return <p>LOADING</p>
+		}
+
+		if (error) {
+			return (
+				<p className='center-text error'>{error}</p>
+			)
+		}
+
 		return(
-			<div>
-				Results
-				<pre>{JSON.stringify(this.props, null, 2)}</pre>
-			</div>
+			<React.Fragment>
+				<div className='grid space-around container-sm'>
+					<Card
+						header={winner.score === loser.score ? 'Tie' : 'Winner'}
+						subheader={`Score: ${winner.score.toLocaleString()}`}
+						avatar={winner.profile.avatar_url}
+						href={winner.profile.html_url}
+						name={winner.profile.login}
+					>
+						<ProfileList profile={winner.profile} />
+					</Card>
+					<Card
+						header={winner.score === loser.score ? 'Tie' : 'Loser'}
+						subheader={`Score: ${loser.score.toLocaleString()}`}
+						avatar={loser.profile.avatar_url}
+						href={loser.profile.html_url}
+						name={loser.profile.login}
+					>
+						<ProfileList profile={loser.profile} />
+					</Card>
+					<button
+						className='btn dark-btn btn-space'
+						onClick={this.props.onReset}
+					>
+						Restart
+					</button>
+				</div>
+			</React.Fragment>
 		)
 	}
+}
+
+Results.propTypes = {
+	playerOne: PropTypes.string.isRequired,
+	playerTwo: PropTypes.string.isRequired,
+	onReset: PropTypes.func.isRequired
 }
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,3 +1,66 @@
+const params = ''
+
+function getErrorMessage(message, username) {
+	if (message === 'Not Found') {
+		return `${username} doesn't exist`
+	}
+
+	return message
+}
+
+function getProfile(username) {
+	return fetch(`https://api.github.com/users/${username}${params}`)
+		.then((res) => res.json())
+		.then((profile) => {
+			if (profile.message) {
+				throw new Error(getErrorMessage(profile.message, username))
+			}
+
+			return profile
+		})
+}
+
+function getRepos(username) {
+	return fetch(`https://api.github.com/users/${username}/repos?per_page=40${params}`)
+		.then((res) => res.json())
+		.then((repos) => {
+			if (repos.message) {
+				throw new Error(getErrorMessage(repos.message, username))
+			}
+
+			return repos
+		})
+}
+
+function getUserData(player) {
+	return Promise.all([
+		getProfile(player),
+		getRepos(player)
+	]).then(([profile, repos]) => ({
+		profile,
+		score: calculateScore(profile.followers, repos)
+	}))
+}
+
+function calculateScore(followers, repos) {
+	return (followers * 3) + getStarCount(repos)
+}
+
+function getStarCount(repos) {
+	return repos.reduce((count, { stargazers_count }) => count + stargazers_count, 0)
+}
+
+function sortPlayers(players) {
+	return players.sort((a,b) => b.score - a.score)
+}
+
+export function battle(players) {
+	return Promise.all([
+		getUserData(players[0]),
+		getUserData(players[1])
+	]).then((results) => sortPlayers(results))
+}
+
 export function fetchPopularRepos(language) {
 	const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
